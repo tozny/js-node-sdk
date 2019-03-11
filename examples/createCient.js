7e9987538d8b2e8e78d89a95675778f0de9cf5f5
Jenna Tucker <jenna.tucker@tozny.com>
2019-03-11 14:15:42 -0700

    init

---
+++ examples/createCient.js
@@ -0,0 +1,14 @@
+const tozStore = require('../dist/index.js')
+
+console.log(tozStore)
+
+// console.log(tozStore.default)
+
+// console.log(tozStore.default.Client)
+// console.log(tozStore.default.Config)
+// console.log(tozStore.default.init)
+
+console.log('CORRECT VALUES')
+console.log(tozStore.Client)
+console.log(tozStore.Config)
+console.log(tozStore.init)
