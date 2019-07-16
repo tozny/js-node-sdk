a93ae5e9e17427db83349c6e2def0df7be5336ea
Jenna Tucker <jenna.tucker@tozny.com>
2019-07-16 14:46:54 -0700

    up to calling writeLargeFile

--- examples/createClient.js
+++ examples/createClient.js
@@ -1,16 +1,39 @@
+require("dotenv").config()
+const fs = require("fs")
+
 const tozStore = require("../dist/index.js")
-
-console.log(tozStore)
-
-// console.log(tozStore.default)
-
-// console.log(tozStore.default.Client)
-// console.log(tozStore.default.Config)
-// console.log(tozStore.default.init)
-
-console.log("CORRECT VALUES")
-console.log(tozStore.Client)
-console.log(tozStore.Config)
+const Config = tozStore.Config
+const config = Config.fromObject(process.env.CLIENT_CREDENTIALS_JSON)
 const Client = tozStore.init("Sodium")
-console.log(Client)
-const client = new Client()
+const client = new Client(config)
+
+const writeFile = async client => {
+  const exampleFile = fs.appendFile(
+    "mynewfile1.txt",
+    "Hello content!",
+    function(err) {
+      if (err) throw err
+      console.log("Saved!")
+    }
+  )
+  //   const exampleFile = new File(
+  //     [
+  //       "A first line of text.  And a second line of text.  Lastly, a third line of text."
+  //     ],
+  //     "filename.txt",
+  //     { type: "text/plain" }
+  //   )
+  const plainMeta = {
+    key1: "val1",
+    key2: "val2"
+  }
+
+  const writtenFile = await client.writeLargeFile(
+    "testFileType",
+    exampleFile,
+    plainMeta
+  )
+  console.log(writtenFile)
+}
+
+writeFile(client)
