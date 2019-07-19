35f7895aafbb9e5c58265ed43b02d9739d2f2d32
Alannah Carr <alannah@tozny.com>
2019-07-19 13:49:44 -0700

    encryptLargeFile added

--- examples/createClient.js
+++ examples/createClient.js
@@ -7,33 +7,33 @@ const config = Config.fromObject(process.env.CLIENT_CREDENTIALS_JSON)
 const Client = tozStore.init("Sodium")
 const client = new Client(config)
 
+const readline = require('readline');
+const stream = require('stream');
+
 const writeFile = async client => {
-  const exampleFile = fs.appendFile(
-    "mynewfile1.txt",
-    "Hello content!",
-    function(err) {
-      if (err) throw err
-      console.log("Saved!")
-    }
-  )
-  //   const exampleFile = new File(
-  //     [
-  //       "A first line of text.  And a second line of text.  Lastly, a third line of text."
-  //     ],
-  //     "filename.txt",
-  //     { type: "text/plain" }
-  //   )
+  let writtenFile = []
   const plainMeta = {
     key1: "val1",
     key2: "val2"
   }
 
-  const writtenFile = await client.writeLargeFile(
-    "testFileType",
-    exampleFile,
-    plainMeta
-  )
-  console.log(writtenFile)
+  // const path = 'examples/prac.txt'
+  // fs.appendFile(path, ' write something.', function(err) {
+  //   if (err) throw err
+  //   console.log("saved!")
+  // })
+
+  try {
+    writtenFile = await client.writeLargeFile(
+      "janeEyre",
+      /*"examples/janeEyre.txt"*/ 'examples/prac.txt',
+      plainMeta
+    )
+  } catch (err) {
+    console.log(err)
+  }
+
+  // console.log(writtenFile)
 }
 
 writeFile(client)
