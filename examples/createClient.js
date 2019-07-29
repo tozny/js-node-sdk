9a6442ddb8e95081aed8181c59ca363029c23f9e
Alannah Carr <alannah@tozny.com>
2019-07-29 16:04:04 -0700

    remove comments

--- examples/createClient.js
+++ examples/createClient.js
@@ -1,14 +1,10 @@
-require("dotenv").config()
-const fs = require("fs")
+require('dotenv').config()
 
 const tozStore = require("../dist/index.js")
 const Config = tozStore.Config
 const config = Config.fromObject(process.env.CLIENT_CREDENTIALS_JSON)
-const Client = tozStore.init("Sodium")
+const Client = tozStore.init('Sodium')
 const client = new Client(config)
-
-// const readline = require('readline');
-// const stream = require('stream');
 
 const writeFile = async client => {
   let writtenFile = []
@@ -17,23 +13,34 @@ const writeFile = async client => {
     key2: "val2"
   }
 
-  // const path = 'examples/prac.txt'
-  // fs.appendFile(path, ' write something.', function(err) {
-  //   if (err) throw err
-  //   console.log("saved!")
-  // })
+  const fileType = "jane-eyre-test"
+  const fileName = 'examples/janeEyre.txt'
 
   try {
     writtenFile = await client.writeLargeFile(
-      "janeEyre",
-      "examples/prac.txt" /*'examples/janeEyre.txt'*/,
+      fileType,
+      fileName,
       plainMeta
     )
   } catch (err) {
     console.log(err)
   }
 
-  // console.log(writtenFile)
+  console.log(writtenFile)
 }
 
-writeFile(client)
+const readFile = async client => {
+  const recordID = '5d98c2f4-22e8-484a-be8d-94ad50940ae8'
+  const destFile = 'jane.txt'
+  try {
+    let decryptedFile = await client.readLargeFile(
+      recordID,
+      destFile
+    )
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+readFile(client)
+// writeFile(client)
